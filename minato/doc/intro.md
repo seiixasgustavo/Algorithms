f7454aa37958514244c01852a54c1d19ba28e9b6
Gustavo Seixas <gunabakbs@gmail.com>
2021-08-22 23:04:34 -0300

    First commit

---
+++ minato/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to minato
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ minato/test/minato/core_test.clj
@@ -0,0 +1,7 @@
+(ns minato.core-test
+  (:require [clojure.test :refer :all]
+            [minato.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
