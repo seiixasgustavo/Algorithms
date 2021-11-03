d10c3a53e1b2ffab4898205929719f55c6bf712e
Gustavo Seixas <gunabakbs@gmail.com>
2021-11-03 17:15:52 -0300

    all commits

--- toge/src/main/java/com/seiixasgustavo/algorithms/one/Tester.java
+++ toge/src/main/java/com/seiixasgustavo/algorithms/one/Tester.java
@@ -1,2 +1,17 @@
-package com.seiixasgustavo.algorithms.one;public class Tester {
+package com.seiixasgustavo.algorithms.one;
+
+public class Tester {
+    public static void main(String[] args) {
+        System.out.println(UniqueString.isUniqueWithDS("dasdsadasdsasda"));
+        System.out.println(UniqueString.isUniqueWithDS("a"));
+        System.out.println(UniqueString.isUniqueWithDS("aa"));
+        System.out.println(UniqueString.isUniqueWithDS("abcdefghijklmnop"));
+
+        System.out.println();
+
+        System.out.println(UniqueString.isUniqueWithoutDS("dasdsadasdsasda"));
+        System.out.println(UniqueString.isUniqueWithoutDS("a"));
+        System.out.println(UniqueString.isUniqueWithoutDS("aa"));
+        System.out.println(UniqueString.isUniqueWithoutDS("abcdefghijklmnop"));
+    }
 }
--- toge/src/main/java/com/seiixasgustavo/algorithms/one/UniqueString.java
+++ toge/src/main/java/com/seiixasgustavo/algorithms/one/UniqueString.java
@@ -1,2 +1,28 @@
-package com.seiixasgustavo.algorithms.one;public class UniqueString {
+package com.seiixasgustavo.algorithms.one;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class UniqueString {
+    public static Boolean isUniqueWithDS(final String str) {
+        final HashMap<Character, Boolean> memo = new HashMap<Character, Boolean>();
+
+        for(final Character c : str.toCharArray()) {
+            if (memo.get(c) != null) return false;
+            memo.put(c, true);
+        }
+
+        return true;
+    }
+
+    public static Boolean isUniqueWithoutDS(final String str) {
+        char[] ca = str.toCharArray();
+        Arrays.sort(ca);
+
+        for(int i = 0; i < ca.length-1; i++) {
+            if (ca[i] == ca[i+1]) return false;
+        }
+
+        return true;
+    }
 }
